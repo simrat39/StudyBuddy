fc66e07de43e3d57f8b11a147150975ce7675a7a
Simrat Grewal <simrats169169@gmail.com>
2022-03-12 20:22:43 -0800

    feat: Add back button to time select page

--- lib/time_select/time_select_page.dart
+++ lib/time_select/time_select_page.dart
@@ -35,45 +35,62 @@ class _TimeSelectPageState extends State<TimeSelectPage> {
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("Set timer for: ",
-                    style: Theme.of(context).textTheme.titleLarge),
-                TextField(
-                  controller: _controller,
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
                 ),
-                Material(
-                  type: MaterialType.transparency,
-                  child: Card(
-                    color: const Color(0xff8A93E9),
-                    child: InkWell(
-                      borderRadius: BorderRadius.circular(12),
-                      onTap: () {
-                        int seconds = int.parse(_controller.value.text);
-                        ChangeNotifierProvider<TimerStateProvider> provider =
-                            ChangeNotifierProvider(
-                          (_) => TimerStateProvider(
-                            duration: Duration(seconds: seconds),
-                          ),
-                        );
-                        widget.callback(provider);
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30.0,
-                          vertical: 16.0,
-                        ),
-                        child: Text("Begin",
-                            style: Theme.of(context).textTheme.titleLarge),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("Set timer for: ",
+                          style: Theme.of(context).textTheme.titleLarge),
+                      TextField(
+                        controller: _controller,
                       ),
-                    ),
+                      Material(
+                        type: MaterialType.transparency,
+                        child: Card(
+                          color: const Color(0xff8A93E9),
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(12),
+                            onTap: () {
+                              int seconds = int.parse(_controller.value.text);
+                              ChangeNotifierProvider<TimerStateProvider>
+                                  provider = ChangeNotifierProvider(
+                                (_) => TimerStateProvider(
+                                  duration: Duration(seconds: seconds),
+                                ),
+                              );
+                              widget.callback(provider);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 30.0,
+                                vertical: 16.0,
+                              ),
+                              child: Text("Begin",
+                                  style:
+                                      Theme.of(context).textTheme.titleLarge),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/timer_page.dart
+++ lib/timer_page.dart
@@ -35,121 +35,125 @@ class TimerPageState extends State<TimerPage> {
           padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 12.0),
           child: Column(
             children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Card(
-                color: const Color(0xffFFE0C3),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 14.0,
-                    vertical: 16.0,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        "Study Session ~ 1hr",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.copyWith(fontSize: 22),
-                      ),
-                      const Spacer(),
-                      const Icon(
-                        Icons.edit,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              TimerText(
-                initial: const Duration(
-                  hours: 1,
-                  minutes: 20,
-                  seconds: 42,
-                ),
-                timerStateNotifier: widget.timerStateChangeNotifier,
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              Card(
-                color: const Color(0xffFFE0C3),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 16.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "5 friends studying with you",
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium
-                            ?.copyWith(fontSize: 22),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const BuddyRow(),
-              const SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              Column(
                 children: [
-                  DoneButton(onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const DonePage(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    color: const Color(0xffFFE0C3),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 14.0,
+                        vertical: 16.0,
                       ),
-                    );
-                  }),
-                  PlayPauseButton(
+                      child: Row(
+                        children: [
+                          Text(
+                            "Study Session ~ 1hr",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Icons.edit,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  TimerText(
+                    initial: const Duration(
+                      hours: 1,
+                      minutes: 20,
+                      seconds: 42,
+                    ),
                     timerStateNotifier: widget.timerStateChangeNotifier,
                   ),
-                  QuitButton(
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => ClipRRect(
-                          borderRadius: BorderRadius.circular(14),
-                          child: AlertDialog(
-                            backgroundColor: Colors.white,
-                            title: const Text("Its sad to see you go!"),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text("Close"),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).popUntil(
-                                    (route) => route.isFirst,
-                                  );
-                                },
-                                child: const Text("Exit"),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+                  const SizedBox(
+                    height: 80,
                   ),
+                  Card(
+                    color: const Color(0xffFFE0C3),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 16.0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "5 friends studying with you",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(fontSize: 22),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const BuddyRow(),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      DoneButton(onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const DonePage(),
+                          ),
+                        );
+                      }),
+                      PlayPauseButton(
+                        timerStateNotifier: widget.timerStateChangeNotifier,
+                      ),
+                      QuitButton(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) => ClipRRect(
+                              borderRadius: BorderRadius.circular(14),
+                              child: AlertDialog(
+                                backgroundColor: Colors.white,
+                                title: const Text("Its sad to see you go!"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Close"),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).popUntil(
+                                        (route) => route.isFirst,
+                                      );
+                                    },
+                                    child: const Text("Exit"),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  )
                 ],
-              )
+              ),
             ],
           ),
         ),
